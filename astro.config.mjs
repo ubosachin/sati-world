bcffd3056b7fc78fc46ba6da0772b0c77002956e
ubosachin <ubosachin@gmail.com>
2024-06-09 18:00:50 +0530

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "",
   integrations: [tailwind(), icon()],
 });
